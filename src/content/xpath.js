b02dee7e04656347f22f7c96e269e4ae1312b54f
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-05-25 23:02:29 -0300

    alterando xpath do botão das imagens

--- src/content/xpath.js
+++ src/content/xpath.js
@@ -1,4 +1,4 @@
 module.exports = {
     search: "/html[1]/body[1]/div[1]/div[3]/form[1]/div[2]/div[1]/div[1]/div[1]/div[2]/input[1]",
-    images: "/html[1]/body[1]/div[6]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/a[1]",
+    images: "//a[contains(text(),'Imagens')]",
 }
